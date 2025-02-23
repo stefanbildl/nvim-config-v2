4cffe30e936b30b5497f9df13c34685454269323
Stefan Bildl <sbildl@eu.sumida.com>
2025-02-23 22:28:03 +0100

    add win32yank for wsl

--- lua/options.lua
+++ lua/options.lua
@@ -40,3 +40,18 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+if vim.fn.executable 'win32yank.exe' == 1 then
+  vim.g.clipboard = {
+    name = 'win32yank',
+    copy = {
+      ['+'] = 'win32yank.exe -i --crlf',
+      ['*'] = 'win32yank.exe -i --crlf',
+    },
+    paste = {
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 0,
+  }
+end

51fd005ecc76d9c1464be1ad58801e0c5940fe02
Stefan Bildl <sbildl@eu.sumida.com>
2025-03-11 12:27:09 +0100

    update + linting

--- init.lua
+++ init.lua
@@ -491,8 +491,6 @@ require('lazy').setup({
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
           ['<CR>'] = cmp.mapping.confirm { select = true },
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           ['<C-k>'] = cmp.mapping {
             i = function()
